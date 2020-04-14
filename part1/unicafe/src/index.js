9cbca347779ed80feba1de7ff3cfcb37b547ae0c
Will Parr <parr.will.08@gmail.com>
2020-04-14 20:22:24 +0100

    Part 1 - Completed exercise 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,7 +7,7 @@ const Button = props => (
   </button>
 )
 
-const Count = ({ text, value }) => {
+const Stat = ({ text, value }) => {
   return (
     <p>{text} {value}</p>
   )
@@ -18,20 +18,55 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+
+  const handleLeftGood = () => {
+    setAll(all + 1)
+    setGood(good + 1)
+  }
+  const handleNeutral = () => {
+    setAll(all + 1)
+    setNeutral(neutral + 1)
+  }
+
+  const handleBad = () => {
+    setAll(all + 1)
+    setBad(bad + 1)
+  }
+
+  const calcAverage = (good, bad, all) => {
+    if (all === 0) {
+      return 0
+    }
+    else {
+      return (good - bad) / all
+    }
+  }
+
+  const calcPercPositive = (good, all) => {
+    let val = 0
+    if (all > 0 & good > 0) {
+      val = 100.0 * (good / all)
+    }
+    return `${val}%`
+  }
 
   return (
     <div>
       <h1>Give Feedback</h1>
       <div>
-        <Button text="good" handleClick={() => setGood(good + 1)} />
-        <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
-        <Button text="bad" handleClick={() => setBad(bad + 1)} />
+        <Button text="good" handleClick={handleLeftGood} />
+        <Button text="neutral" handleClick={handleNeutral} />
+        <Button text="bad" handleClick={handleBad} />
       </div>
       <div>
         <h1>Statistics</h1>
-          <Count text="good" value={good}/>
-          <Count text="neutral" value={neutral}/>
-          <Count text="bad" value={bad}/>
+        <Stat text="good" value={good} />
+        <Stat text="neutral" value={neutral} />
+        <Stat text="bad" value={bad} />
+        <Stat text="all" value={all} />
+        <Stat text="average" value={calcAverage(good, bad, all)} />
+        <Stat text="positive" value={calcPercPositive(good, all)} />
       </div>
     </div>
   )
