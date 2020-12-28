1de379152e4aeed199105b3c61ada4da6c5b9e2b
Will Parr <parr.will.08@gmail.com>
2020-12-28 15:08:29 +0000

    Create InputForm component

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,53 +1,15 @@
 import React, { useState } from "react";
 import ContactList from "./components/ContactList";
+import InputForm from "./components/InputForm";
 
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-1234567" },
   ]);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
-
-  const addNewName = (event) => {
-    event.preventDefault();
-
-    const personExists = persons.find((person) => person.name === newName);
-    if (personExists) {
-      window.alert(`${newName} is already added to phonebook`);
-    } else {
-      const newPersonEntry = {
-        name: newName,
-        number: newNumber,
-      };
-
-      setPersons(persons.concat(newPersonEntry));
-      setNewName("");
-      setNewNumber("")
-    }
-  };
-
-  const handleNewName = (event) => {
-    setNewName(event.target.value);
-  };
-
-  const handleNewNumber = (event) => {
-    setNewNumber(event.target.value);
-  };
 
   return (
     <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addNewName}>
-        <div>
-          name: <input value={newName} onChange={handleNewName} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNewNumber} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <InputForm persons={persons} setPersons={setPersons} />
       <ContactList persons={persons} />
     </div>
   );
---
+++ part2/phonebook/src/components/InputForm.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+const InputForm = ({ persons, setPersons }) => {
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+
+  const addNewName = (event) => {
+    event.preventDefault();
+
+    const personExists = persons.find((person) => person.name === newName);
+    if (personExists) {
+      window.alert(`${newName} is already added to phonebook`);
+    } else {
+      const newPersonEntry = {
+        name: newName,
+        number: newNumber,
+      };
+
+      setPersons(persons.concat(newPersonEntry));
+      setNewName("");
+      setNewNumber("");
+    }
+  };
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNewNumber = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewName}>
+        <div>
+          name: <input value={newName} onChange={handleNewName} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNewNumber} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default InputForm;
