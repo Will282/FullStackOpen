c549abd1d19fd112a2313911defb2f7f9b68b319
Will Parr <parr.will.08@gmail.com>
2020-12-29 11:45:39 +0000

    Finished Ex 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import ContactList from "./components/ContactList";
 import InputForm from "./components/InputForm";
 import Filter from "./components/Filter";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [searchName, setSearchName] = useState("");
+
+  const hook = () => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  };
+  useEffect(hook, []);
 
   const handleSearchName = (event) => {
     setSearchName(event.target.value.toLowerCase());
